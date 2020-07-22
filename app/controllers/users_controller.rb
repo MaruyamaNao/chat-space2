3cdd16e3c6aca730bd8917183b1babad5275466b
MaruyamaNao <61174627+MaruyamaNao@users.noreply.github.com>
2020-07-22 21:05:07 +0900

    コントローラー　edit/updetaアクション　
    変更できた
    →root_path
    
    変更できなかった
    →render :edit
    editのビューを再度描画

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def edit
+  end
+
+  def update
+    if current_user.update(params.require(:user).permit(:name, :email))
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 end
